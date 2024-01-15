6580140c5b041bad2b36a8872f5073c2636f239f
melina ghassabi <149719824+Melinaghassabi@users.noreply.github.com>
2024-01-15 22:07:15 +0330

    homeworke8_|1

---
+++ homeworke8_1.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <climits>
+using namespace std;
+
+ int main() {
+  int n = 18;
+   int n,max = INT_MTN, min = INT_MAX, a;
+    cout <<"Enter 10 Numbers: ";
+	 for (int i = 0; i <n; i++)
+	 {
+	 	cin>>num;
+	 	{
+	 		max= num;
+		 }
+         if (num<min) 
+		 { 
+		 max = num;
+		 }
+		      }
+			  
+			  cout <<"Maximum number is:" <<max << endl;
+			  cout<<"minimum number is :" <<min<<endl;
+			  a= max + min;
+			  cout<<"average of max and min is:" <<a/2;
+			  
+			  
+			  
+			  
+			  
+			  
+			  return0: }
